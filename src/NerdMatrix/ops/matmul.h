7ed720da7ed44c5ee5f99b9cae974a89079840c9
kiddos <kiddo831007@gmail.com>
2017-05-17 22:44:28 +0800

    add matrix multiplication

--- src/NerdMatrix/ops/matmul.h
+++ src/NerdMatrix/ops/matmul.h
@@ -10,12 +10,19 @@ namespace matrix {
 namespace ops {
 
 template <typename T>
-void MatMul(const NerdMatrix<T>& m1, const NerdMatrix<T>& m2,
-            NerdMatrix<T>& result) {
-  if (m1.cols() != m2.rows()) {
+void MatMul(const NerdMatrix<T>& a, const NerdMatrix<T>& b, NerdMatrix<T>& c) {
+  if (a.cols() != b.rows()) {
     throw except::IllegalOpsException(
         "Matrix Multiplication: Input Size does not match.");
   } else {
+    int m = a.rows();
+    int k = a.cols();
+    int n = b.cols();
+    c = NerdMatrix<T>(m, n);
+
+    T alpha = static_cast<T>(1);
+    T beta = static_cast<T>(0);
+    wrapper::Gemm(m, n, k, alpha, a.data(), b.data(), beta, &c[0]);
   }
 }
 
---
+++ test/test_nerdmatrix_matmul.cc
@@ -0,0 +1,39 @@
+#include <gtest/gtest.h>
+#include <complex>
+#include "NerdMatrix/except/nerdmatrix_exception.h"
+#include "NerdMatrix/matrix.h"
+#include "NerdMatrix/ops/matmul.h"
+
+using std::complex;
+using nerd::matrix::NerdMatrix;
+
+template <typename T>
+class TestNerdMatrixMatMul : public ::testing::Test {
+ public:
+  void TestMatMul() {
+    int m = 1024;
+    int n = 1024;
+    int k = 1024;
+    NerdMatrix<T> a(m, k);
+    NerdMatrix<T> b(k, n);
+    NerdMatrix<T> c(m, n);
+    nerd::matrix::ops::MatMul(a, b, c);
+
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        T value = T{0};
+        for (int l = 0; l < k; ++l) {
+          value += a[i * k + l] * b[l * n + j];
+        }
+        EXPECT_LT(std::abs(c[i * n + j] - value), 1e-4);
+      }
+    }
+  }
+};
+
+typedef ::testing::Types<float, double, complex<float>, complex<double>> TestTypes;
+TYPED_TEST_CASE(TestNerdMatrixMatMul, TestTypes);
+
+TYPED_TEST(TestNerdMatrixMatMul, MatMul) {
+  this->TestMatMul();
+}
