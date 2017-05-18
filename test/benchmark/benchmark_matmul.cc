fa7bbaae715ebfea847b5a037ca997ad77ffa02f
kiddos <kiddo831007@gmail.com>
2017-05-18 14:34:14 +0800

    renaming

--- test/benchmark/benchmark_matmul.cc
+++ test/benchmark/benchmark_matmul.cc
@@ -1,12 +1,12 @@
 #include <benchmark/benchmark.h>
-#include <complex>
-#include "NerdMatrix/ops/matmul.h"
 #include "NerdMatrix/matrix.h"
+#include "NerdMatrix/ops/matmul.h"
 
+using std::complex;
 using nerd::matrix::NerdMatrix;
 
 template <typename T>
-void BenchMarkMatrixMul(benchmark::State& state) {
+void BenchmarkMatMul(benchmark::State& state) {
   int size = state.range();
   NerdMatrix<T> a(size, size);
   NerdMatrix<T> b(size, size);
@@ -15,27 +15,27 @@ void BenchMarkMatrixMul(benchmark::State& state) {
     nerd::matrix::ops::MatMul(a, b, c);
   }
   state.SetBytesProcessed(int64_t(state.iterations()) *
-              int64_t(state.range(0)));
+                          int64_t(state.range(0)));
 }
 
-BENCHMARK_TEMPLATE(BenchMarkMatrixMul, float)
-  ->Unit(benchmark::kMicrosecond)
-  ->RangeMultiplier(2)
-  ->Range(1 << 0, 1 << 10);
+BENCHMARK_TEMPLATE(BenchmarkMatMul, float)
+    ->Unit(benchmark::kMicrosecond)
+    ->RangeMultiplier(2)
+    ->Range(1 << 0, 1 << 10);
 
-BENCHMARK_TEMPLATE(BenchMarkMatrixMul, double)
-  ->Unit(benchmark::kMicrosecond)
-  ->RangeMultiplier(2)
-  ->Range(1 << 0, 1 << 10);
+BENCHMARK_TEMPLATE(BenchmarkMatMul, double)
+    ->Unit(benchmark::kMicrosecond)
+    ->RangeMultiplier(2)
+    ->Range(1 << 0, 1 << 10);
 
-BENCHMARK_TEMPLATE(BenchMarkMatrixMul, std::complex<float>)
-  ->Unit(benchmark::kMicrosecond)
-  ->RangeMultiplier(2)
-  ->Range(1 << 0, 1 << 10);
+BENCHMARK_TEMPLATE(BenchmarkMatMul, complex<float>)
+    ->Unit(benchmark::kMicrosecond)
+    ->RangeMultiplier(2)
+    ->Range(1 << 0, 1 << 10);
 
-BENCHMARK_TEMPLATE(BenchMarkMatrixMul, std::complex<double>)
-  ->Unit(benchmark::kMicrosecond)
-  ->RangeMultiplier(2)
-  ->Range(1 << 0, 1 << 10);
+BENCHMARK_TEMPLATE(BenchmarkMatMul, complex<double>)
+    ->Unit(benchmark::kMicrosecond)
+    ->RangeMultiplier(2)
+    ->Range(1 << 0, 1 << 10);
 
 BENCHMARK_MAIN();
