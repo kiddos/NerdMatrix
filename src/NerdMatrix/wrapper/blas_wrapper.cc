2e672bd90420c1f51aebefdc72141debe78b4dfe
kiddos <kiddo831007@gmail.com>
2017-05-30 23:38:12 +0800

    add more wrapper functions

--- src/NerdMatrix/wrapper/blas_wrapper.cc
+++ src/NerdMatrix/wrapper/blas_wrapper.cc
@@ -6,24 +6,27 @@ namespace nerd {
 namespace matrix {
 namespace wrapper {
 
+typedef std::complex<float> complexf;
+typedef std::complex<double> complexd;
+
 template <>
-void Copy<float>(int n, const float* const src, float* dst) {
+void Copy(int n, const float* const src, float* dst) {
   cblas_scopy(n, src, 1, dst, 1);
 }
 
 template <>
-void Copy<double>(int n, const double* const src, double* dst) {
+void Copy(int n, const double* const src, double* dst) {
   cblas_dcopy(n, src, 1, dst, 1);
 }
 
 template <>
-void Copy<std::complex<float>>(int n, const std::complex<float>* const src,
-                               std::complex<float>* dst) {
+void Copy(int n, const complexf* const src,
+                               complexf* dst) {
   cblas_ccopy(n, src, 1, dst, 1);
 }
 
 template <>
-void Copy<std::complex<double>>(int n, const std::complex<double>* const src,
+void Copy(int n, const std::complex<double>* const src,
                                 std::complex<double>* dst) {
   cblas_zcopy(n, src, 1, dst, 1);
 }
@@ -39,8 +42,8 @@ void Axpy(int n, double alpha, const double* const src, double* dst) {
 }
 
 template <>
-void Axpy(int n, std::complex<float> alpha,
-          const std::complex<float>* const src, std::complex<float>* dst) {
+void Axpy(int n, complexf alpha,
+          const complexf* const src, complexf* dst) {
   cblas_caxpy(n, &alpha, src, 1, dst, 1);
 }
 
@@ -48,6 +51,70 @@ template <>
 void Axpy(int n, std::complex<double> alpha,
           const std::complex<double>* const src, std::complex<double>* dst) {
   cblas_zaxpy(n, &alpha, src, 1, dst, 1);
+}
+
+template <>
+void Swap(int n, float* x, float* y) {
+  cblas_sswap(n, x, 1, y, 1);
+}
+
+template <>
+void Swap(int n, double* x, double* y) {
+  cblas_dswap(n, x, 1, y, 1);
+}
+
+template <>
+void Swap(int n, complexf* x, complexf* y) {
+  cblas_cswap(n, x, 1, y, 1);
+}
+
+template <>
+void Swap(int n, complexd* x, complexd* y) {
+  cblas_zswap(n, x, 1, y, 1);
+}
+
+template <>
+void Scale(int n, float alpha, float* x) {
+  cblas_sscal(n, alpha, x, 1);
+}
+
+template <>
+void Scale(int n, double alpha, double* x) {
+  cblas_dscal(n, alpha, x, 1);
+}
+
+template <>
+void Scale(int n, complexf alpha, complexf* x) {
+  cblas_cscal(n, &alpha, x, 1);
+}
+
+template <>
+void Scale(int n, complexd alpha, complexd* x) {
+  cblas_zscal(n, &alpha, x, 1);
+}
+
+template <>
+float Dot(int n, const float* x, const float* y) {
+  return cblas_sdot(n, x, 1, y, 1);
+}
+
+template <>
+double Dot(int n, const double* x, const double* y) {
+  return cblas_ddot(n, x, 1, y, 1);
+}
+
+template <>
+complexf Dot(int n, const complexf* x, const complexf* y) {
+  complexf c;
+  cblas_cdotc_sub(n, x, 1, y, 1, &c);
+  return c;
+}
+
+template <>
+complexd Dot(int n, const complexd* x, const complexd* y) {
+  complexd c;
+  cblas_zdotc_sub(n, x, 1, y, 1, &c);
+  return c;
 }
 
 template <>
@@ -65,10 +132,10 @@ void Gemm(int m, int n, int k, double alpha, const double* const a,
 }
 
 template <>
-void Gemm(int m, int n, int k, std::complex<float> alpha,
-          const std::complex<float>* const a,
-          const std::complex<float>* const b, std::complex<float> beta,
-          std::complex<float>* c) {
+void Gemm(int m, int n, int k, complexf alpha,
+          const complexf* const a,
+          const complexf* const b, complexf beta,
+          complexf* c) {
   cblas_cgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, &alpha, a, k,
               b, n, &beta, c, n);
 }
--- src/NerdMatrix/wrapper/blas_wrapper.h
+++ src/NerdMatrix/wrapper/blas_wrapper.h
@@ -12,6 +12,15 @@ template <typename T>
 void Axpy(int n, T alpha, const T* const src, T* dst);
 
 template <typename T>
+void Swap(int n, T* x, T* y);
+
+template <typename T>
+void Scale(int n, T alpha, T* x);
+
+template <typename T>
+T Dot(int n, const T* x, const T* y);
+
+template <typename T>
 void Gemm(int m, int n, int k, T alpha, const T* const a, const T* const b,
           T beta, T* dst);
 
